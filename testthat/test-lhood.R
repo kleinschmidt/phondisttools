a065f7b1083597bb7b26ca72ed779d9dab26696d
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-05-22 15:05:07 -0400

    fix dependencies (importFrom and new purrrlyr package)

--- testthat/test-lhood.R
+++ testthat/test-lhood.R
@@ -11,21 +11,21 @@ test_that("Log likelihood is log of non-log likelihood", {
 
 
 
-models <- map(-5:5, ~ list(mu = c(., .), Sigma = diag(c(1,1))))
+models <- purrr::map(-5:5, ~ list(mu = c(., .), Sigma = diag(c(1,1))))
 dat <- c(0,0)
 
 test_that("Marginal likelihood is mean of individual likelihoods", {
 
   expect_equal(marginal_model_lhood(models, dat, log=FALSE),
-               mean(map_dbl(models, ~ model_lhood(., dat))))
+               mean(purrr::map_dbl(models, ~ model_lhood(., dat))))
   expect_equal(marginal_model_lhood(models, dat, log=TRUE),
-               log(mean(exp(map_dbl(models, ~ model_lhood(., dat, log=TRUE))))))
+               log(mean(exp(purrr::map_dbl(models, ~ model_lhood(., dat, log=TRUE))))))
 
 })
 
 test_that("Marginal likelihood defaults to log=TRUE", {
 
   expect_equal(marginal_model_lhood(models, dat),
-               log(mean(exp(map_dbl(models, ~ model_lhood(., dat, log=TRUE))))))
+               log(mean(exp(purrr::map_dbl(models, ~ model_lhood(., dat, log=TRUE))))))
 
 })
